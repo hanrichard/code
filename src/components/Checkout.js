4ad779241625c7964d260598f19b49c266d69998
richardhan <hxy.314@gmail.com>
2021-01-17 16:42:19 +1100

    feat(alert): add alert message

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import { Alert, AlertTitle } from '@material-ui/lab';
 
 const CheckoutWrapper = styled.div`
   width: 100%;
@@ -8,11 +10,31 @@ const CheckoutWrapper = styled.div`
   background-color: #fff;
 `;
 
+const CheckoutMessage = styled.div`
+  margin-top: 50px;
+`;
+
 const Checkout = ({ cart }) => {
-  console.log(cart);
+  const handleClick = () => {
+    console.log('xx');
+  };
   return cart.cart.length > 0 && (
     <CheckoutWrapper>
-      Check out
+      <Button variant="contained" onClick={handleClick}>Checkout</Button>
+      <CheckoutMessage>
+        <Alert severity="error">
+          <AlertTitle>Error</AlertTitle>
+          This is an error alert —
+          {' '}
+          <strong>check it out!</strong>
+        </Alert>
+        <Alert severity="success">
+          <AlertTitle>Success</AlertTitle>
+          This is a success alert —
+          {' '}
+          <strong>check it out!</strong>
+        </Alert>
+      </CheckoutMessage>
     </CheckoutWrapper>
   );
 };
